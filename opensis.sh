90a7dc592ac409b667bd5bbc8130e6091a674e3a
Vinood Katuwa <vinod.katuwa12@gmail.com>
2017-08-13 12:37:20 +0530

    Update opensis.sh

--- opensis.sh
+++ opensis.sh
@@ -1,15 +1,11 @@
-sudo apt-get update
-sudo apt-get install apache2
-sudo apt-get install mysql-server 
-sudo apt-get install php5
-sudo apt-get install libapache2-mod-php5
-sudo apt-get install php5-mysql
- 
-wget http://nchc.dl.sourceforge.net/project/opensis-ce/opensis5.1.zip
-sudo apt-get install unzip
-unzip opensis5.1.zip
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install unzip apache2 mysql-server php libapache2-mod-php php-mysql
+sleep 05
+wget https://excellmedia.dl.sourceforge.net/project/opensis-ce/opensis6.4.zip
+unzip opensis6.4.zip
 sudo cp -R  ~/opensis /var/www/
 chmod -R  777 /var/www/opensis/
+chown -R www-data:www-data /var/www/opensis
 sudo service apache2 restart
 
 Open Browser [http://localhost/opensis/install/index.php] and proceed with installation.
